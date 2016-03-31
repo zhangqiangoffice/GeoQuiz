1e5e89acdfa7b30ca915ca7edd0b2676969638d4
zhangqiangoffice <zhangqiangoffice@163.com>
2016-03-31 20:56:12 +0800

    优化 上一个、下一个按钮

--- src/com/example/geoquiz/QuizActivity.java
+++ src/com/example/geoquiz/QuizActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -12,8 +13,8 @@ public class QuizActivity extends Activity {
 
 	private Button mTrueButton;
 	private Button mFalseButton;
-	private Button mNextButton;
-	private Button mPrevButton;
+	private ImageButton mNextButton;
+	private ImageButton mPrevButton;
 	private TextView mQuestionTextView;
 	
 	private TrueFalse[] mQuestionBank = new TrueFalse[] {
@@ -81,7 +82,7 @@ public class QuizActivity extends Activity {
 			}
 		});
 		
-		mNextButton = (Button) findViewById(R.id.next_button);
+		mNextButton = (ImageButton) findViewById(R.id.next_button);
 		mNextButton.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
@@ -91,12 +92,12 @@ public class QuizActivity extends Activity {
 			}
 		});
 		
-		mPrevButton = (Button) findViewById(R.id.prev_button);
+		mPrevButton = (ImageButton) findViewById(R.id.prev_button);
 		mPrevButton.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
-				mCurrentIndex = (mCurrentIndex - 1) % mQuestionBank.length;
+				mCurrentIndex = (mCurrentIndex - 1 + mQuestionBank.length) % mQuestionBank.length;
 				updateQuestion();
 				
 			}
